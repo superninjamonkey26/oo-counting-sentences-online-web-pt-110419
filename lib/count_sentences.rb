a134475609d706470bc23122f5509ed571ed5d04
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-03 01:47:46 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x|x.empty?}.size
   end
 end

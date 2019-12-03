dcd596b0f4641bae6bda4e045ed541b79d4ca6de
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-03 01:35:48 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,31 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.count_sentences
   end
 end
